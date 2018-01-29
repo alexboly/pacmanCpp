6dd98ab3329a982abfae70597a5ad80ef809f34c
Alexandru Bolboaca <alexboly@gmail.com>
2018-01-29 15:01:21 +0200

    Refactor first test

--- pacmanTests.cpp
+++ pacmanTests.cpp
@@ -1,10 +1,22 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
+#include <iostream>
 
 using namespace std;
 
+const string before(const string& line, const char& element){
+    const int position = line.find(element);
+    return line.substr(0, position);
+}
+
+const string after(const string& line, const char& element){
+    const int position = line.find(element);
+    return line.substr(position + 1);
+}
+
 const string tick(const string& initialLine){
-    return ".... >...";
+    const string afterAfterMove = after(initialLine, '>').substr(1);
+    return before(initialLine, '>') + ' ' + '>' + afterAfterMove;
 }
 
 TEST_CASE("pacman eats the dot on the right when it has dots on the right and it's oriented towards right"){
