846ab79b0517d57d65e9cf44e982e254c7e7448d
Alexandru Bolboaca <alexboly@gmail.com>
2018-01-29 18:14:16 +0200

    Improve readability by introducing Line type

--- pacmanTests.cpp
+++ pacmanTests.cpp
@@ -4,43 +4,45 @@
 
 using namespace std;
 
+typedef string Line;
+
 enum class KindOfToken : char {
     Empty = ' ',
     Dot = '.',
     Pacman = '>'
 };
 
-const string before(const string& line, const char& element){
+const Line before(const Line& line, const char& element){
     const int position = line.find(element);
     return line.substr(0, position);
 }
 
-auto beforePacman = [](const string& line){
+auto beforePacman = [](const Line& line){
     return before(line, '>');
 };
 
-const string after(const string& line, const char& element){
+const Line after(const Line& line, const char& element){
     const int position = line.find(element);
     return line.substr(position + 1);
 }
 
-auto afterPacman = [](const string& line){
+auto afterPacman = [](const Line& line){
     return after(line, '>');
 };
 
-const string tick(const string& initialLine){
-    const string afterAfterMove = afterPacman(initialLine).substr(1);
+const Line tick(const Line& initialLine){
+    const Line afterAfterMove = afterPacman(initialLine).substr(1);
     return beforePacman(initialLine) + ' ' + '>' + afterAfterMove;
 }
 
-const string dots(const int count){
+const Line dots(const int count){
     vector<char> theDots(count, '.');
-    return string(theDots.begin(), theDots.end());
+    return Line(theDots.begin(), theDots.end());
 }
 
 TEST_CASE("pacman eats the dot on the right when it has dots on the right and it's oriented towards right"){
-    string lineBeforeMove =           dots(4) + (char)KindOfToken::Pacman + dots(4);
-    string expectedLineAfterMove =    dots(4) + (char)KindOfToken::Empty + (char)KindOfToken::Pacman + dots(3);
+    Line lineBeforeMove =           dots(4) + (char)KindOfToken::Pacman + dots(4);
+    Line expectedLineAfterMove =    dots(4) + (char)KindOfToken::Empty + (char)KindOfToken::Pacman + dots(3);
 
     auto lineAfterMove = tick(lineBeforeMove);
 
