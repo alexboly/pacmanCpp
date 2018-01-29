e48305ea3c10a81e92449477ef5ce0f40bed69b5
Alexandru Bolboaca <alexboly@gmail.com>
2018-01-29 14:31:07 +0200

    First failing test (removed all extra stuff, kept only the most important thing)

--- pacmanTests.cpp
+++ pacmanTests.cpp
@@ -3,41 +3,15 @@
 
 using namespace std;
 
-enum class KindOfToken : char {
-    PacmanRight = '>',
-    Empty = '\0'
-};
-
-string lineOfDots(int dotsCount){
-    return "";
-}
-
-string makeBoard(string line){
-    return "";
-}
-
-string tick(string board){
-    return "";
+const string tick(const string& initialLine){
+    return initialLine;
 }
 
 TEST_CASE("pacman eats the dot on the right when it has dots on the right and it's oriented towards right"){
-    int beforeDotsCount = 10;
-    int afterDotsCount = 10;
-    auto lineWithPacman = lineOfDots(beforeDotsCount) + (char)KindOfToken::PacmanRight + lineOfDots(afterDotsCount);
-    auto expectedLineWithPacman = lineOfDots(beforeDotsCount) + (char)KindOfToken::Empty + (char)KindOfToken::PacmanRight + lineOfDots(afterDotsCount - 1);
-    auto initialBoard = makeBoard(lineWithPacman);
-    auto expectedFinalBoard = makeBoard(expectedLineWithPacman);
+    string lineBeforeMove =           "....>....";
+    string expectedLineAfterMove =    ".... >...";
 
-    auto boardAfterMove = tick(initialBoard);
+    auto lineAfterMove = tick(lineBeforeMove);
 
-    CHECK(boardAfterMove == expectedFinalBoard);
-
-    /*
-    beforeDotsCount << (0..<50)
-    afterDotsCount << Gen.integer(1..100).take(50)
-    totalDotsCount = beforeDotsCount + afterDotsCount + 1
-    beforeLineCount << (0..<50)
-    afterLineCount << (0..<50)
-    makeBoard = this.&makeBoardWithLineWithPacman.curry(beforeLineCount).rcurry(afterLineCount)
-    */
+    CHECK(lineAfterMove == expectedLineAfterMove);
 }
