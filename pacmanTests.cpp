c4e28a837efe21b3b12bad67ca7666ef302a30c6
Alexandru Bolboaca <alexboly@gmail.com>
2018-01-29 15:15:54 +0200

    Remove duplication using curry (through lambdas)

--- pacmanTests.cpp
+++ pacmanTests.cpp
@@ -9,14 +9,22 @@ const string before(const string& line, const char& element){
     return line.substr(0, position);
 }
 
+auto beforePacman = [](const string& line){
+    return before(line, '>');
+};
+
 const string after(const string& line, const char& element){
     const int position = line.find(element);
     return line.substr(position + 1);
 }
 
+auto afterPacman = [](const string& line){
+    return after(line, '>');
+};
+
 const string tick(const string& initialLine){
-    const string afterAfterMove = after(initialLine, '>').substr(1);
-    return before(initialLine, '>') + ' ' + '>' + afterAfterMove;
+    const string afterAfterMove = afterPacman(initialLine).substr(1);
+    return beforePacman(initialLine) + ' ' + '>' + afterAfterMove;
 }
 
 TEST_CASE("pacman eats the dot on the right when it has dots on the right and it's oriented towards right"){
