2d07664600163aece67ee80435ac72e8f60ab231
Alexandru Bolboaca <alexboly@gmail.com>
2018-02-05 18:09:40 +0200

    Fix edge case for moving at the end of the line

--- pacmanTests.cpp
+++ pacmanTests.cpp
@@ -11,52 +11,8 @@ enum class KindOfToken : char {
     Pacman = '>'
 };
 
+
 typedef vector<KindOfToken> Line;
-
-const Line operator+(const Line& line, const KindOfToken& token){
-    Line newLine(line);
-    newLine.push_back(token);
-    return newLine;
-}
-
-const Line operator+(const Line& first, const Line& second){
-    Line newLine(first);
-    newLine.insert(newLine.end(), second.begin(), second.end());
-    return newLine;
-}
-
-const Line before(const Line& line, const KindOfToken& element){
-    auto position = find(line.begin(), line.end(), element);
-    return Line(line.begin(), position);
-}
-
-auto beforePacman = [](const Line& line){
-    return before(line, KindOfToken::Pacman);
-};
-
-const Line after(const Line& line, const KindOfToken& element){
-    auto position = find(line.begin(), line.end(), element);
-    return Line(position + 1, line.end());
-}
-
-auto afterPacman = [](const Line& line){
-    return after(line, KindOfToken::Pacman);
-};
-
-const Line tick(const Line& initialLine){
-    const Line after = afterPacman(initialLine);
-    const Line afterAfterMove(after.begin() + 1, after.end());
-    return (
-            beforePacman(initialLine) + 
-            KindOfToken::Empty + 
-            KindOfToken::Pacman + 
-            afterAfterMove
-           );
-}
-
-const Line dots(const int count){
-    return Line(count, KindOfToken::Dot);
-}
 
 template<typename SourceCollection, typename DestinationCollection, typename Lambda>
 const DestinationCollection allocateAndTransform(const SourceCollection& source, Lambda&& transformation){
@@ -95,6 +51,56 @@ const string to_string_with_foreach(const Line& line){
     return value;
 }
 
+
+const Line operator+(const Line& line, const KindOfToken& token){
+    Line newLine(line);
+    newLine.push_back(token);
+    return newLine;
+}
+
+const Line operator+(const Line& first, const Line& second){
+    Line newLine(first);
+    newLine.insert(newLine.end(), second.begin(), second.end());
+    return newLine;
+}
+
+const Line before(const Line& line, const KindOfToken& element){
+    auto position = find(line.begin(), line.end(), element);
+    return Line(line.begin(), position);
+}
+
+auto beforePacman = [](const Line& line){
+    return before(line, KindOfToken::Pacman);
+};
+
+const Line after(const Line& line, const KindOfToken& element){
+    auto position = find(line.begin(), line.end(), element);
+    return Line(position + 1, line.end());
+}
+
+auto afterPacman = [](const Line& line){
+    return after(line, KindOfToken::Pacman);
+};
+
+const Line removeFirst(const Line& initialLine){
+    return (initialLine.size() == 0) ?  
+        Line() :
+        Line(initialLine.begin() + 1, initialLine.end());
+}
+
+const Line tick(const Line& initialLine){
+    return (
+            beforePacman(initialLine) + 
+            KindOfToken::Empty + 
+            KindOfToken::Pacman + 
+            removeFirst(afterPacman(initialLine)) 
+           );
+}
+
+const Line dots(const int count){
+    return (count < 0) ? Line() : Line(count, KindOfToken::Dot);
+}
+
 void propertyTest(const int dotsBefore, const int dotsAfter){
      cout << "START for dots before: " << dotsBefore << " and dots after " << dotsAfter << endl;
     Line lineBeforeMove =           dots(dotsBefore) + KindOfToken::Pacman + dots(dotsAfter);
@@ -111,9 +117,16 @@ void propertyTest(const int dotsBefore, const int dotsAfter){
 
 TEST_CASE("pacman eats the dot on the right when it has dots on the right and it's oriented towards right"){
     vector<int> dotsBefore({0, 1, 4});
-    vector<int> dotsAfter( {1, 1, 4});
+    vector<int> dotsAfter( {0, 1, 4});
 
     for(int i = 0; i < dotsBefore.size(); ++i){
         propertyTest(dotsBefore[i], dotsAfter[i]);
     }
+}
+
+TEST_CASE("after pacman works"){
+    CHECK(dots(0) == Line());
+    CHECK(dots(0) + dots(0) == Line());
+    CHECK(afterPacman( dots(0) + KindOfToken::Pacman + dots(0)) == dots(0));
+    CHECK(beforePacman( dots(0) + KindOfToken::Pacman + dots(0)) == dots(0));
 }
